d8898f34da5aa39d5899868f88b13262d56bd227
bulletdrop <88382910+bulletdrop@users.noreply.github.com>
2022-07-21 23:30:58 +0200

    Finished example

---
+++ example.py
@@ -0,0 +1,59 @@
+import rapid_auth
+import json
+print("Press 1 for Sign Up and 2 for Sign In")
+
+input_1 = input()
+
+signed_in = False
+
+while signed_in == False:
+    if input_1 == "1":
+        print("Enter Username: ")
+        username = input()
+        print("Enter Password: ")
+        password = input()
+        json_response = json.loads(rapid_auth.sign_up_user(username, password))
+        if json_response["status"] == "success":
+            print("Successfully Signed Up")
+            signed_in = True
+        else:
+            print("Failed to Sign Up")
+            print("Error: " + json_response["message"])
+    elif  input_1 == "2":
+        print("Enter Username: ")
+        username = input()
+        print("Enter Password: ")
+        password = input()
+        json_response = json.loads(rapid_auth.sign_in_user(username, password))
+        if json_response["status"] == "success":
+            print("Successfully Signed In")
+            signed_in = True
+        else:
+            print("Failed to Sign In")
+            print("Error: " + json_response["message"])
+
+print("Press 1 to get active keys and 2 redeem a key")
+input_2 = input()
+if input_2 == "1":
+    json_response = json.loads(rapid_auth.get_active_keys(username, password))
+    if json_response["status"] == "success":
+        counter = 1
+        for product in json_response["products"]:
+            print("Product " + str(counter) + ": " + product["product_name"])
+            print("Days left: " + str(product["days_left"]))
+            print("Lifetime: " + str(product["lifetime"]))
+            print("Key name: " + product["key_name"])
+            print("\n\n")
+            counter += 1
+    else:
+        print("Failed to get active keys")
+        print("Error: " + json_response["message"])
+elif input_2 == "2":
+    print("Enter License Key: ")
+    license_key = input()
+    json_response = json.loads(rapid_auth.redeem_license_key(username, password, license_key))
+    if json_response["status"] == "success":
+        print("Successfully Redeemed License Key")
+    else:
+        print("Failed to Redeem License Key")
+        print("Error: " + json_response["message"])
--- rapid_auth.py
+++ rapid_auth.py
@@ -5,11 +5,70 @@ import requests
 
 _hwid = hwid.get_hwid()
 
-def login_user(username, password):
-    url = 'http://localhost/testing/decrypt.php'
-    post_data = {'encryptedtext': 'AwHKiHBpAtxeUr6s9WxMsVgd1DEjA/1FfJmwG0m9nTWfHVm9bNSTV54DXseb1M+R8SM2/SgrkwP4PPdZzND3YaJxKTG4/aZ7bm/kZHSkLZACeQ==',
-    'password': 'this-is-my-password'}
+def sign_in_user(username, password):
+    url = 'http://localhost/rapid_auth_api/loader_users/sign_in.php'
+    post_data = {'api_key': config.api_key,
+    'username': crypting.encrypt_data(username),
+    'password': crypting.encrypt_data(password),
+    'windows_username': crypting.encrypt_data(_hwid["windows_username"]),
+    'gpu_name': crypting.encrypt_data(_hwid["gpu_name"]),
+    'gpu_ram': crypting.encrypt_data(_hwid["gpu_memory"]),
+    'drive_count': crypting.encrypt_data(str(_hwid["drive_count"])),
+    'cpu_name': crypting.encrypt_data(_hwid["cpu_name"]),
+    'cpu_cores': crypting.encrypt_data(str(_hwid["cpu_cores"])),
+    'os_caption': crypting.encrypt_data(_hwid["os_caption"]),
+    'os_serial_number': crypting.encrypt_data(_hwid["os_serial_number"])
+    }
 
     x = requests.post(url, data = post_data)
 
-    print(x.text)
+    return crypting.decrypt_data(x.text)
+
+
+
+def sign_up_user(username, password):
+    url = 'http://localhost/rapid_auth_api/loader_users/sign_up.php'
+    post_data = {'api_key': config.api_key,
+    'username': crypting.encrypt_data(username),
+    'password': crypting.encrypt_data(password),
+    'windows_username': crypting.encrypt_data(_hwid["windows_username"]),
+    'gpu_name': crypting.encrypt_data(_hwid["gpu_name"]),
+    'gpu_ram': crypting.encrypt_data(_hwid["gpu_memory"]),
+    'drive_count': crypting.encrypt_data(str(_hwid["drive_count"])),
+    'cpu_name': crypting.encrypt_data(_hwid["cpu_name"]),
+    'cpu_cores': crypting.encrypt_data(str(_hwid["cpu_cores"])),
+    'os_caption': crypting.encrypt_data(_hwid["os_caption"]),
+    'os_serial_number': crypting.encrypt_data(_hwid["os_serial_number"])
+    }
+
+    x = requests.post(url, data = post_data)
+
+    return  crypting.decrypt_data(x.text)
+
+
+
+def get_active_keys(username, password):
+    url = 'http://localhost/rapid_auth_api/loader_users/get_active_keys.php'
+    post_data = {'api_key': config.api_key,
+    'username': crypting.encrypt_data(username),
+    'password': crypting.encrypt_data(password)
+    }
+
+    x = requests.post(url, data = post_data)
+
+    return crypting.decrypt_data(x.text)
+
+
+
+def redeem_license_key(username, password, license_key):
+    url = 'http://localhost/rapid_auth_api/loader_users/redeem_key.php'
+    post_data = {'api_key': config.api_key,
+    'username': crypting.encrypt_data(username),
+    'password': crypting.encrypt_data(password),
+    'license_key': crypting.encrypt_data(license_key)
+    }
+
+    x = requests.post(url, data = post_data)
+
+    return crypting.decrypt_data(x.text)
+
